df6757a1946ca6e07a5446c5390ab59b199ea924
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2026-01-08 11:01:09 +0700

    pscore With (In) R Software

---
+++ pscore/pscore.R
@@ -0,0 +1,15 @@
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Propensity score estimation Use pscore With (In) R Software
+install.packages("olsrr")
+library("olsrr")
+# Estimation Propensity score estimation Use pscore With (In) R Software
+pscore = read.csv("https://raw.githubusercontent.com/timbulwidodostp/pscore/main/pscore/pscore.csv", sep = ";")
+pscore <- pscore(data = pscore, formula = therapie ~ tgr + age)
+summary(pscore)
+# Propensity score estimation Use pscore With (In) R Software
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Finished
